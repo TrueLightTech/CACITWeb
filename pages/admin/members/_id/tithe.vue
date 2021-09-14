40011c2225e6df17e0f4388484f596d7b33b373a
Maxwell Cofie <maxwell@hubtel.com>
2021-09-14 15:13:52 +0000

    merged

--- pages/admin/members/_id/tithe.vue
+++ pages/admin/members/_id/tithe.vue
@@ -13,22 +13,37 @@
 
 
         <div class="row justify-content-center my-4">
-          <div class="col-md-10">
-            <div class="d-flex justify-content-between">
-              <div class="dropdown w-100">
-                <button class="btn btn-primary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton1"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                  Year
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                  <li @click="setYear(year)" v-for="(year,index) in generateArrayOfYears()" :key="index"><a
-                    class="dropdown-item" href="#">
-                    {{year}}
-                  </a></li>
-                </ul>
+          <div class="col-md-6">
+            <div class="d-flex justify-content-end">
+              <div class="d-flex justify-content-end">
+                <div class="dropdown w-100">
+                  <button class="btn btn-primary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton1"
+                          data-bs-toggle="dropdown" aria-expanded="false">
+                    Month
+                  </button>
+                  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li @click="setMonth(month)" v-for="(month,index) in months" :key="index"><a
+                      class="dropdown-item" href="#">
+                      {{month}}
+                    </a></li>
+                  </ul>
+                </div>
+
+                <div class="dropdown mx-2  w-100">
+                  <button class="btn btn-primary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton1"
+                          data-bs-toggle="dropdown" aria-expanded="false">
+                    Year
+                  </button>
+                  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li @click="setYear(year)" v-for="(year,index) in generateArrayOfYears()" :key="index"><a
+                      class="dropdown-item" href="#">
+                      {{year}}
+                    </a></li>
+                  </ul>
+                </div>
               </div>
 
-              <button type="button" class="btn btn-success w-25" disabled>Save</button>
+              <button type="button" class="btn btn-success w-25 d-none" disabled>Save</button>
 
             </div>
           </div>
@@ -36,30 +51,22 @@
 
 
       </div>
-      <div class="col-md-10">
+      <div class="col-md-6">
         <div class="table-responsive">
           <table class="table table-bordered border-primary">
             <thead>
-            <tr>
-              <th scope="col">Months</th>
-              <th scope="col">Week 1</th>
-              <th scope="col">Week 2</th>
-              <th scope="col">Week 3</th>
-              <th scope="col">Week 4</th>
-              <th scope="col">Week 5</th>
-              <th scope="col">Total</th>
+            <tr class="text-center">
+              <th scope="col">Weeks</th>
+              <th scope="col">Amount (GHS)</th>
+              <th scope="col"></th>
             </tr>
             </thead>
             <tbody>
-            <tr v-for="i in months">
-              <th scope="row">{{i}}</th>
-              <td><input type="number" style="height: 50px;width: 100px;" class="w-100 form-control m-0 p-0"/></td>
-              <td><input type="number" style="height: 50px;width: 100px;" class="w-100 form-control m-0 p-0"/></td>
-              <td><input type="number" style="height: 50px;width: 100px;" class="w-100 form-control m-0 p-0"/></td>
-              <td><input type="number" style="height: 50px;width: 100px;" class="w-100 form-control m-0 p-0"/></td>
-              <td><input type="number" style="height: 50px;width: 100px;" class="w-100 form-control m-0 p-0"/></td>
-              <td><input disabled type="number" style="height: 50px;width: 100px;"
-                         class="w-100 form-control m-0 p-0"/>
+            <tr class="text-center" v-for="i in weeks">
+              <th scope="row"><h2>{{i}}</h2></th>
+              <td><h2>5,000</h2></td>
+              <td>
+                <button class="btn btn-success"><i class="fa fa-plus-circle"></i> Record</button>
               </td>
             </tr>
             </tbody>
@@ -83,6 +90,7 @@
         user: ChurchMember,
         month: 10,
         year: "2021",
+        weeks: ["1", "2", "3", "4", "5", "Total"],
         months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
       }
     },
@@ -93,6 +101,8 @@
     methods: {
       setYear(yr) {
         this.year = yr
+      },
+      setMonth(mth) {
       },
       generateArrayOfYears() {
         let max = new Date().getFullYear()
