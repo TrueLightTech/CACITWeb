aceca516db620a67ef2bce97e20a113a7e6c8839
Maxwell Cofie <maxwell@hubtel.com>
2021-10-10 07:42:21 +0000

    added search by phonenumbers to members table

--- components/Members.vue
+++ components/Members.vue
@@ -28,13 +28,13 @@
               <i class="fa fa-search" style="color:#cdcdcd;"></i>
             </span>
             <input type="text" v-model="searchQuery" @keyup="searchByName()" class="form-control"
-                   placeholder="Search Member"
+                   placeholder="Search Member by name or phone number"
                    aria-label="Username"
                    aria-describedby="basic-addon1">
 
           </div>
           <table class="table table-hover table-responsive">
-            <caption>List of members <span class="badge rounded-pill bg-primary">{{totalCount}}</span>
+            <caption>List of members <span class="badge rounded-pill bg-primary">{{ totalCount }}</span>
             </caption>
             <thead>
             <tr>
@@ -47,7 +47,7 @@
             </thead>
             <tbody>
             <tr v-for="(member,index) in members.results" :key="index">
-              <th scope="row">{{index+1}}</th>
+              <th scope="row">{{ index + 1 }}</th>
               <td>
                 <NuxtLink :to="'members/'+member.phoneNumber+'/view'">
                   <img :src="getProfileImage(member.profilePicture)" class="img-fluid profileImage rounded-circle">
@@ -55,10 +55,10 @@
               </td>
               <td>
                 <NuxtLink :to="'members/'+member.phoneNumber+'/view'">
-                  {{member.name}}
+                  {{ member.name }}
                 </NuxtLink>
               </td>
-              <td>{{member.churchFamilyName}}</td>
+              <td>{{ member.churchFamilyName }}</td>
               <td class="text-end">
                 <!-- Example single danger button -->
                 <div class="btn-group">
@@ -95,9 +95,9 @@
               <li class="page-item">
                 <a @click="fetchMembers(currentPage - 1)" class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
               </li>
-<!--              <li v-for="index in numberOfPages" :key="index" class="page-item">-->
-<!--                <a @click="fetchMembers(index)" class="page-link" href="#">{{index}}</a>-->
-<!--              </li>-->
+              <!--              <li v-for="index in numberOfPages" :key="index" class="page-item">-->
+              <!--                <a @click="fetchMembers(index)" class="page-link" href="#">{{index}}</a>-->
+              <!--              </li>-->
               <li class="page-item">
                 <a @click="fetchMembers(currentPage+1)" class="page-link" href="#">Next</a>
               </li>
@@ -113,104 +113,111 @@
 </template>
 
 <script>
-  import {mapGetters} from 'vuex'
-  import PageLoader from "./PageLoader";
-  import {MemberList} from "../network/Member";
-  import {profileImageBaseUrl} from "../resources/constants";
+import {mapGetters} from 'vuex'
+import PageLoader from "./PageLoader";
+import {MemberList} from "../network/Member";
+import {profileImageBaseUrl} from "../resources/constants";
 
 
-  export default {
-    name: "Members",
-    components: {PageLoader},
-    props: ['isActive'],
-    data() {
-      return {
-        numberOfPages: 0,
-        currentPage: 0,
-        totalCount: 0,
-        searchQuery: '',
-        isLoading: false,
-        toDeleteId: '',
-        members: MemberList
-      }
-    },
-    watch: {
-      isActive: function (newVal, oldVal) { // watch it
-        if (newVal) {
-          this.fetchMembers()
-        }
-      }
-    },
-    beforeMount() {
-      this.fetchMembers()
-    },
-    methods: {
-      searchByName() {
-        // this.isLoading = true
-        this.$axios.get(`churchmembers?Name=${this.searchQuery}`).then(response => {
-          this.members = Object.assign(MemberList, response.data.data)
-          this.numberOfPages = this.members.totalPages
-          this.totalCount = this.members.totalCount
-
-        }).catch(error => {
-          // this.isLoading = false
-        })
-      },
-      fetchMembers(page = 1, pageSize = 10) {
-        this.currentPage = page
-        this.isLoading = true
-
-        let familyId = this.loggedInUser.data.churchFamilyId
-
-        let filterByFamilyId = ''
-        if (this.loggedInUser.data.roleId === '2') {
-          filterByFamilyId = `&FamilyId=${familyId}`
-        }
-
-
-        this.$axios.get(`churchmembers?Page=${page}&PageSize=${pageSize}${filterByFamilyId}`).then(response => {
-          this.members = Object.assign(MemberList, response.data.data)
-          this.numberOfPages = this.members.totalPages
-          this.totalCount = this.members.totalCount
-
-          this.isLoading = false
-        }).catch(error => {
-          this.isLoading = false
-        })
-      },
-      deleteMember(id) {
-        this.$axios.delete(`churchmembers/${id}`).then(response => {
-          console.log(response.data)
-          this.fetchMembers()
-          this.$toast.info("User successfully deleted.")
-        }).catch(error => {
-          console.log(error)
-        })
-      },
-      getProfileImage(image) {
-        if (image) {
-          return `${profileImageBaseUrl}/${image}`
-        }
-        return require(`~/assets/imgs/user.svg`)
-      },
-      modalState(data) {
-        if (data.toString().toLowerCase() === 'positive') {
-          this.deleteMember(this.toDeleteId)
-        }
-      },
-      checkUserToDelete(id) {
-        this.toDeleteId = id
-      }
-    },
-    computed: {
-      ...mapGetters(['isAuthenticated', 'loggedInUser'])
+export default {
+  name: "Members",
+  components: {PageLoader},
+  props: ['isActive'],
+  data() {
+    return {
+      numberOfPages: 0,
+      currentPage: 0,
+      totalCount: 0,
+      searchQuery: '',
+      isLoading: false,
+      toDeleteId: '',
+      members: MemberList
     }
+  },
+  watch: {
+    isActive: function (newVal, oldVal) { // watch it
+      if (newVal) {
+        this.fetchMembers()
+      }
+    }
+  },
+  beforeMount() {
+    this.fetchMembers()
+  },
+  methods: {
+    searchByName() {
+
+      let filterBy = ''
+      if(!isNaN(this.searchQuery)){
+        filterBy = `Number=${this.searchQuery}`
+      }else{
+        filterBy = `Name=${this.searchQuery}`
+      }
+
+      this.$axios.get(`churchmembers?${filterBy}`).then(response => {
+        this.members = Object.assign(MemberList, response.data.data)
+        this.numberOfPages = this.members.totalPages
+        this.totalCount = this.members.totalCount
+
+      }).catch(error => {
+        // this.isLoading = false
+      })
+    },
+    fetchMembers(page = 1, pageSize = 10) {
+      this.currentPage = page
+      this.isLoading = true
+
+      let familyId = this.loggedInUser.data.churchFamilyId
+
+      let filterByFamilyId = ''
+      if (this.loggedInUser.data.roleId === '2') {
+        filterByFamilyId = `&FamilyId=${familyId}`
+      }
+
+
+      this.$axios.get(`churchmembers?Page=${page}&PageSize=${pageSize}${filterByFamilyId}`).then(response => {
+        this.members = Object.assign(MemberList, response.data.data)
+        this.numberOfPages = this.members.totalPages
+        this.totalCount = this.members.totalCount
+
+        this.isLoading = false
+      }).catch(error => {
+        this.isLoading = false
+      })
+    },
+    deleteMember(id) {
+      this.$axios.delete(`churchmembers/${id}`).then(response => {
+        console.log(response.data)
+        this.fetchMembers()
+        this.$toast.info("User successfully deleted.")
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    getProfileImage(image) {
+      if (image) {
+        return `${profileImageBaseUrl}/${image}`
+      }
+      return require(`~/assets/imgs/user.svg`)
+    },
+    modalState(data) {
+      if (data.toString().toLowerCase() === 'positive') {
+        this.deleteMember(this.toDeleteId)
+      }
+    },
+    checkUserToDelete(id) {
+      this.toDeleteId = id
+    }
+  },
+  computed: {
+    ...mapGetters(['isAuthenticated', 'loggedInUser'])
   }
+}
 </script>
 
 <style scoped>
-  .profileImage {
-    width: 50px;
-    height: 50px;
-  }
+.profileImage {
+  width: 50px;
+  height: 50px;
+}
 </style>
