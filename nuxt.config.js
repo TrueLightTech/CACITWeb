af765651e150f764661ca813fb851be8c1973568
Isaac <isaacanane9@gmail.com>
2024-04-26 19:49:11 +0000

    resolved not found route

--- nuxt.config.js
+++ nuxt.config.js
@@ -97,7 +97,11 @@ export default {
     }
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    babel: {
+      compact: true
+    }
+  },
   router: {
     middleware: ['auth']
   },
